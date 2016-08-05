2490e7d8a7690fe2ab73855c90cf6769beb66205
Tony Mai <tonymaibox@yahoo.com>
2016-08-05 14:02:48 -0400

    teamwork makes the dreamwork

--- challenges.md
+++ challenges.md
@@ -1,8 +1,17 @@
 1. Take a look at the code that's in this directory. Answer the following questions:
   * What is a Gemfile?
-  * What's the purpose of the `environment.rb` file in the `config` directory?
+  	- file/container or "required" for the project to work.
+
+  * What's the purpose of the `environment.rb`
+  file in the `config` directory?
+  - That basically brings/pulls together all the files necessary to run files of the project. Brings in databases if necessary. Sets up the environment for project to run.
+
   * What's the purpose of the `console.rb` file? Why might that be useful?
+  - It allows the user to enter into pry via console.
+
   * We have a database called `tweets.db`. Given what you've seen in the code, how might we execute SQL statements on this DB?
+  - Create an .sql file to run SQL statements
+
   * Our `Tweet` has an initialize method. How many arguments does it take?
 
 2. Write a method on our tweet class called `find` that takes in an ID number as an argument and returns an instance of our Tweet class that corresponds with that row in the database.
@@ -12,3 +21,13 @@ For example:
   tweet = Tweet.find(1)
   #=> #<Tweet:0x007fcee3bf9870 @id=1, @message="good coffee", @username="coffeedad">
 ```
+
+tweet = Tweet.new
+tweet.username = 'cher'
+tweet.message = 'Somethin weird'
+tweet.id #=> nil
+tweet.save
+tweet.id #=> 2
+tweet.message = 'Something else weird'
+tweet.save
+tweet.id #= 2 should be the same row in the database
--- lib/tweet.rb
+++ lib/tweet.rb
@@ -1,10 +1,99 @@
 class Tweet
   attr_accessor :message, :username, :id
-  
-  def initialize(options)
-    @message = options[:message]
-    @username = options[:username]
-    @username = options[:id]
+
+  def self.all
+  	sql = <<-SQL
+  	SELECT * 
+  	FROM tweets;
+  	SQL
+
+  	results = DB[:conn].execute(sql)
+  	# results.collect do |result|
+  	# 	Tweet.new(result)
+ 	self.new_from_rows(results)
   end
 
+  def self.new_from_rows(rows)
+	  	rows.collect do |result|
+			Tweet.new(result)
+		end
+	end
+
+	def self.find(id)
+		sql = <<-SQL
+		SELECT *
+		FROM tweets
+		WHERE id = ?;
+		SQL
+
+		result = DB[:conn].execute(sql, id)
+		if result.empty?
+			puts "No Tweet Found"
+		else
+			self.new(result.first)
+		end
+	end
+
+  def initialize(options={})
+    @message = options['message']
+    @username = options['username']
+    @id = options['id']
+  end
+
+  def save
+  	# make a call to the database to create a row with a message and a username value
+  	# sql = <<-SQL
+  	#IF STATEMENT
+ #  	IF NOT EXISTS (SELECT id 
+ #  	FROM tweets 
+ #  	WHERE id IS NULL)
+ #  	BEGIN
+	#   	INSERT INTO tweets (username, message)
+	#   	VALUES (?, ?)
+	# END
+ #  	ELSE
+ #  	BEGIN
+	#   	UPDATE tweets
+	#   	SET message = ?
+	# END
+	# ;
+ #  	SQL
+
+
+
+
+ 	if self.id == nil
+ 		 	sqlnew = <<-SQL
+		 	INSERT INTO tweets (username, message)
+			VALUES (?, ?);
+		 	SQL
+
+			results = DB[:conn].execute(sqlnew, self.username, self.message)
+		  	
+		  	sql = <<-SQL
+		  	SELECT id
+		  	FROM tweets
+		  	ORDER BY id DESC
+		  	LIMIT 1;
+		  	SQL
+  			
+  			resultid = DB[:conn].execute(sql)
+  			
+  			self.id = resultid.first['id']
+  	
+		  	# return the tweet
+		  	self
+	else
+		 	sqlupd = <<-SQL
+		 	UPDATE tweets
+		 	SET message = ?
+		 	WHERE id = ?;
+		 	SQL
+		
+			results = DB[:conn].execute(sqlupd, self.message, self.id)
+	end
+
+  	# find the row that was just inserted and set the id from that row to this tweets id
+
+end
 end
